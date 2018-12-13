3818ec041e048ef3db1ba55c1f308f7cf85fbc3b
Matt Lehman <mhlehman24@gmail.com>
2018-12-13 16:58:06 -0500

    Update examples.R

--- examples.R
+++ examples.R
@@ -1,28 +1,29 @@
-# ## Examples
-# library(ggmattle24)
-# library(ggplot2)
-# library(extrafont)
-#
-# # Bar Graph ---------------------------------------------------------------
-#
-# ggplot(mtcars, aes(x = gear, y = wt)) +
-#   geom_col() +
-#   labs(
-#     title = 'How does gear affect weight?'
-#     ,x = 'Gear'
-#     ,y = 'Weight'
-#   ) +
-#   theme_mattle24()
-#
-#
-#
-# # Scatterplot -------------------------------------------------------------
-#
-# ggplot(mtcars, aes(x = wt, y = mpg)) +
-#   geom_point() +
-#   labs(
-#     title = 'How does weight affect MPG?'
-#     ,x = 'Weight'
-#     ,y = 'MPG'
-#   ) +
-#   theme_mattle24()
+## Examples
+library(mattle24utils)
+library(ggplot2)
+library(extrafont)
+
+# Sample Plot
+
+ggplot(mtcars, aes(x = gear, y = wt)) +
+  geom_col() +
+  labs(
+    title = 'How does gear affect weight?'
+    ,x = 'Gear'
+    ,y = 'Weight'
+  ) +
+  facet_wrap(~am) +
+  theme_mattle24()
+
+
+
+# Scatterplot -------------------------------------------------------------
+
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  labs(
+    title = 'How does weight affect MPG?'
+    ,x = 'Weight'
+    ,y = 'MPG'
+  ) +
+  theme_mattle24()
