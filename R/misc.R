61f7d8f4b418ed133db86e8fe7c9de8597bf9f90
Matt Lehman <mhlehman24@gmail.com>
2019-01-11 18:08:57 -0500

    add iff not null

---
+++ R/misc.R
@@ -0,0 +1,30 @@
+# Miscellaneous functions
+
+#' Iff not null
+#'
+#' Returns the first argument if and only if not null,
+#' else returns the second arguement. Comes from Hadley's
+#' Advanced R: https://adv-r.hadley.nz.
+#'
+#' @param arg1 the first argument. Will be returned iff
+#' not null.
+#' @param arg2 the second argument.
+#'
+#' @examples
+#' \dontrun {
+#' x <- NULL
+#' x %||% 'default'
+#'
+#' x <- 'option1'
+#' x %||% 'default'
+#' }
+#'
+#' @rdname iff_not_null
+#'
+#' @export
+`%||%` <- function(arg1, arg2) {
+  if (!is.null(arg1))
+    arg1
+  else
+    arh2
+}
