bf8f27470633334bdbefed3d9969605290ed0b45
Matt Lehman <mhlehman24@gmail.com>
2019-01-14 18:28:36 -0500

    add the skeleton docstring function

---
+++ R/addins.R
@@ -0,0 +1,29 @@
+# Addins for RStudio
+
+# This is weird because roxygen2 wants to read this string as an roxygen
+# comment, so instead I need to sub `#'` in after.
+function_docstring <- glue::glue(
+  "
+  {special} Title
+  {special}
+  {special} Description
+  {special}
+  {special} @param
+  {special}
+  {special} @export
+  "
+, special = "#'")
+
+current_row <- function() {
+  rstudioapi::getActiveDocumentContext()$selection[[1]][[1]][[1]][1]
+}
+
+#' Insert Skeleton Docstring
+#'
+#' Inserts a skeleton docstring for a function at the current line if in a
+#' document or in the console if used in the console.
+#'
+#' @export
+insert_docstring <- function() {
+  rstudioapi::insertText(c(current_row(), 1), paste0(function_docstring, "\n"))
+}
