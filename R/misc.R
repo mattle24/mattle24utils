9b04ec71ecadff231466071e997bd34065d3caf4
Matt Lehman <mhlehman24@gmail.com>
2019-02-26 11:45:48 -0500

    add filter verbose

--- R/misc.R
+++ R/misc.R
@@ -1,7 +1,7 @@
 #' Drop NA verbose
 #'
 #' A wraper of \code{\link{tidyr::drop_na()}} to drop NA values and give a
-#' warning for how many values were dropped.
+#' warning for how many rows were dropped.
 #'
 #' @param .data A data frame or similar object.
 #' @param ... A selection of columns. If empty, all variables are selected. You
@@ -27,7 +27,38 @@ drop_na_verbose <- function(.data, ...) {
   .data <- tidyr::drop_na(.data, ...)
 
   end_nrow <- nrow(.data)
-  warning(starting_nrow - end_nrow, " missing values.", call. = FALSE)
+  warning("Droped", starting_nrow - end_nrow, " rows.", call. = FALSE)
+
+  .data
+}
+
+
+#' Filter verbose
+#'
+#' A wraper of \code{\link{dplyr::filter()}} to give a warning for how many rows
+#' were dropped.
+#'
+#' @param .data A data frame or similar object.
+#' @param ... Logical predicates defined in terms of the variables in .data.
+#'   Multiple conditions are combined with &. Only rows where the condition
+#'   evaluates to TRUE are kept.
+#'
+#' @examples
+#' \dontrun{
+#' set.seed(0)
+#' data <- tibble::tibble(.rows = 40)
+#' data$x1 <- sample(c(NA_real_, 1:4), 40, replace= TRUE)
+#' filter_verbose(data, x1 > 0.5)
+#' }
+#'
+#' @export
+filter_verbose <- function(.data, ...) {
+  starting_nrow <- nrow(.data)
+
+  .data <- dplyr::filter(.data, ...)
+
+  end_nrow <- nrow(.data)
+  warning("Droped", starting_nrow - end_nrow, " rows.", call. = FALSE)
 
   .data
 }
